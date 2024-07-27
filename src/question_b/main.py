a4a40fba8d4e9a809bacc3526ff486367bb55c97
anglpnc01 <angel.ponce1@g.austincc.edu>
2024-07-27 19:37:21 +0000

    final question_b

--- src/question_b/main.py
+++ src/question_b/main.py
@@ -1,1 +1,17 @@
-#add import
+from question_b import create_multiplication_table, display_multiplication_table
+
+def main():
+
+    while True:
+    
+        new_table = create_multiplication_table()
+
+        display_multiplication_table(new_table)
+
+        user_input = input("Do you want to view the table again? (yes/no): ").strip().lower()
+
+        if user_input not in ['yes', 'y']:
+            print("Exiting...")
+            exit()
+
+main()
--- src/question_b/question_b.py
+++ src/question_b/question_b.py
@@ -1,1 +1,22 @@
 #write functions here, don't add input('') statements here!
+
+def create_multiplication_table(total = 10): #total of 10x10 by default!!
+    #2_D list
+
+    list = []
+    
+    for i in range(1, total + 1):
+        
+        row = [i * j for j in range(1, total + 1)] #from 1 to 'total'
+        
+        list.append(row)
+    
+    return list
+
+def display_multiplication_table(list):
+
+    for row in list:
+
+        print(" ".join(f"{num:4}" for num in row))
+
+
