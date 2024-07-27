28851fa24a903a6c7ebd171d84261d1f48480374
anglpnc01 <angel.ponce1@g.austincc.edu>
2024-07-27 19:12:57 +0000

    midterm question_a

--- src/question_a/main.py
+++ src/question_a/main.py
@@ -1,1 +1,35 @@
 #add import
+from question_a import stock_purchase_history
+
+def display_menu():
+
+    print('1 - Display Stock Purchase History: ')
+    print('2 - Exit')
+
+def run_menu():
+
+    choice == '1'
+
+    while(choice != '3'):
+
+        display_menu()
+
+        choice = input('Enter option: ').strip()
+
+        handle_menu()
+
+def handle_menu(choice):
+
+    if(choice == '1')    :
+
+        select_1()
+
+    else:
+        print('Exiting..')
+        exit()
+
+def select_1():
+
+    result = stock_purchase_history
+
+    print(f'Recent stock purchases are: {result}')
--- src/question_a/question_a.py
+++ src/question_a/question_a.py
@@ -28,7 +28,7 @@ class Stock:
         stocks = { 'Stock 1': stock1, 'Stock 2': stock2, 
                   'Stock 3': stock3, 'Stock 4': stock4, 'Stock 5': stock5}
         
-        for key, stock in stocks.items()
+        for key, stock in stocks.items():
             print((f"{key}: Symbol = {stock.get_symbol()}, Company Name = {stock.get_company_name()}"))
 
             
